ea5dd9072da772f74837664349c3b70b81d1224f
Ahmad Rizky Fadhilah <rspnn@gmail.com>
2025-08-17 22:49:59 +0700

    make test pass

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -64,8 +64,6 @@ describe('Login spec', () => {
       .contains(/^Login$/)
       .click();
 
-    cy.get('nav')
-      .contains(/^Sign Out$/)
-      .should('be.visible');
+    cy.get('a[href="/new"]').should('be.visible');
   });
 });
